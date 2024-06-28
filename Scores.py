574e2b7c8f2d52b619bf611230649ec65f0f35a2
Rvej <37398463+Rvej@users.noreply.github.com>
2024-06-28 19:22:03 +0800

    Update Scores.py

--- Scores.py
+++ Scores.py
@@ -526,7 +526,7 @@ def IC50(predictpeptide, length, trainpeptide, matrix):
                     continue
                 PSSMscore += dict_AAPSSM[eachseq[i]][i]
             bindingscore = PSSMscore / int(length)
-            IC50score = math.pow(50000, (0.8 - bindingscore) )/ 1.6
+            IC50score = math.pow(50000, (0.8 - bindingscore) / 1.6)
             dict_trainIC50.setdefault('pos', []).append((eachseq, IC50score))
         dict_trainIC50['neg'] = OrderedDict()
         for eachseq in negtrainpep:
